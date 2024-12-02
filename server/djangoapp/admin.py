3b1f3bf3f218fad2302ea26e4e7466e58c1c43ac
Sinensis1511 <saramartignoni@yahoo.it>
2024-12-02 14:24:20 +0100

    Laboratorio pratico: Creare servizi proxy Django di API di backend

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,16 @@
-# from django.contrib import admin
-# from .models import related models
-
-
-# Register your models here.
+from django.contrib import admin
+from .models import CarMake, CarModel
 
 # CarModelInline class
-
+class CarModelInline(admin.ModelAdmin):
+    fields = ('name', 'description')
 # CarModelAdmin class
-
+class CarModelAdmin(admin.ModelAdmin):
+    fields = ('name')
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
 
-# Register models here
+# Register your models here.
+admin.site.register(CarModel)
+admin.site.register(CarMake)
